a67e4709a86191a78bb837810443088011996dd3
Zhan Rongrui <2742392377@qq.com>
2023-12-03 17:17:46 +0800

    ✨ feat: add split_dataset_by_class

--- tools/dataset.py
+++ tools/dataset.py
@@ -2,7 +2,8 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pickle
 from rich.progress import Progress, track
-
+import random
+from torch.utils.data import Subset, ConcatDataset
 
 class FaceFeaturesDataset(Dataset):
     def __init__(self, features_dict):
@@ -17,23 +18,41 @@ class FaceFeaturesDataset(Dataset):
         return len(self.items)
     
 
-def split_dataset(dataset, num_classes=130, *, folds=10):
-
-    class_indices = [[] for _ in range(num_classes)]
-    for idx, (_, label) in enumerate(dataset):
-        class_indices[label].append(idx)
-        
+def split_dataset(dataset, *, folds=10):
     dataset_size = len(dataset)
     single_split_size = dataset_size // folds
-    splits = [single_split_size] * 9
-    splits.append(dataset_size - sum(splits))
     for i in range(folds):
         # 生成当前折的测试集和训练集的索引
         test_indices = list(range(i * single_split_size, (i + 1) * single_split_size))
         train_indices = list(range(0, i * single_split_size)) + list(range((i + 1) * single_split_size, dataset_size))
         
         # 根据索引创建数据子集
-        train_subset = torch.utils.data.Subset(dataset, train_indices)
-        test_subset = torch.utils.data.Subset(dataset, test_indices)
+        train_subset = Subset(dataset, train_indices)
+        test_subset = Subset(dataset, test_indices)
         
         yield train_subset, test_subset
+
+
+def split_dataset_by_class(dataset, num_classes=130, *, folds=10):
+    class_indices = [[] for _ in range(num_classes)]
+    for idx, (_, label) in enumerate(dataset):
+        class_indices[label].append(idx)
+    for i in range(folds):
+        class_train_subsets = []
+        class_test_subsets = []
+        for indices in class_indices:
+            class_subset = Subset(dataset, indices)
+            subset_size = len(class_subset)
+            single_split_size = subset_size // folds
+            
+            test_indices = list(range(i * single_split_size, (i + 1) * single_split_size))
+            train_indices=list(range(0, i * single_split_size)) + list(range((i + 1) * single_split_size, subset_size))
+
+            class_train_subsets.append(Subset(class_subset, train_indices))
+            class_test_subsets.append(Subset(class_subset, test_indices))
+
+        # 根据索引创建数据子集
+        train_subset = ConcatDataset(class_train_subsets)
+        test_subset = ConcatDataset(class_test_subsets)
+        yield train_subset, test_subset
+    
