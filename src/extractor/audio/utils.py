3670791ab72fefb6454ca31ab024a2694e5f1ce2
cchxhh <148213557+cchxhh@users.noreply.github.com>
2024-03-16 20:08:58 +0800

    add audio variable length networks (#2)
    * add audio variable_length networks
    
    * add audio report
    
    * add vl_feature
    
    * add vl_feature and add model

--- src/extractor/audio/utils.py
+++ src/extractor/audio/utils.py
@@ -5,6 +5,53 @@ from typing import Sequence
 
 import opensmile
 
+# def get_all_audio_methods(ignore_methods: Sequence[str] = ()):
+#     for feature_set, feature_level in product(opensmile.FeatureSet, opensmile.FeatureLevel):
+#         if feature_set.name in ignore_methods or feature_level.name in ignore_methods:
+#             continue
+#         if (
+#             feature_level == opensmile.FeatureLevel.LowLevelDescriptors_Deltas
+#             and feature_set != opensmile.FeatureSet.ComParE_2016
+#         ):
+#             continue
+
+#          match feature_set, feature_level:
+#     case opensmile.FeatureSet.ComParE_2016, opensmile.FeatureLevel.Functionals:
+#         feature_size = 6373
+#     case opensmile.FeatureSet.ComParE_2016, _:
+#         feature_size = 65
+#     case opensmile.FeatureSet.GeMAPS | opensmile.FeatureSet.GeMAPSv01b, opensmile.FeatureLevel.Functionals:
+#         feature_size = 62
+#     case (
+#         opensmile.FeatureSet.GeMAPS
+#         | opensmile.FeatureSet.GeMAPSv01b,
+#         opensmile.FeatureLevel.LowLevelDescriptors,
+#     ):
+#         feature_size = 18
+#     case (
+#         opensmile.FeatureSet.eGeMAPS
+#         | opensmile.FeatureSet.eGeMAPSv01b,
+#         opensmile.FeatureLevel.LowLevelDescriptors,
+#     ):
+#         feature_size = 23
+#     case (
+#         opensmile.FeatureSet.eGeMAPS
+#         | opensmile.FeatureSet.eGeMAPSv01b
+#         | opensmile.FeatureSet.eGeMAPSv02,
+#         opensmile.FeatureLevel.Functionals,
+#     ):
+#         feature_size = 88
+#     case opensmile.FeatureSet.eGeMAPSv02, opensmile.FeatureLevel.LowLevelDescriptors:
+#         feature_size = 25
+#     case opensmile.FeatureSet.emobase, opensmile.FeatureLevel.Functionals:
+#         feature_size = 988
+#     case opensmile.FeatureSet.emobase, opensmile.FeatureLevel.LowLevelDescriptors:
+#         feature_size = 26
+#     case _:
+#         raise ValueError(f"unknown feature_set: {feature_set}/{feature_level}")
+
+# yield opensmile.Smile(feature_set=feature_set, feature_level=feature_level), feature_size
+
 
 def get_all_audio_methods(ignore_methods: Sequence[str] = ()):
     for feature_set, feature_level in product(opensmile.FeatureSet, opensmile.FeatureLevel):
@@ -16,39 +63,114 @@ def get_all_audio_methods(ignore_methods: Sequence[str] = ()):
         ):
             continue
 
-        match feature_set, feature_level:
-            case opensmile.FeatureSet.ComParE_2016, opensmile.FeatureLevel.Functionals:
-                feature_size = 6373
-            case opensmile.FeatureSet.ComParE_2016, _:
-                feature_size = 65
-            case opensmile.FeatureSet.GeMAPS | opensmile.FeatureSet.GeMAPSv01b, opensmile.FeatureLevel.Functionals:
-                feature_size = 62
-            case (
-                opensmile.FeatureSet.GeMAPS
-                | opensmile.FeatureSet.GeMAPSv01b,
-                opensmile.FeatureLevel.LowLevelDescriptors,
-            ):
-                feature_size = 18
-            case (
-                opensmile.FeatureSet.eGeMAPS
-                | opensmile.FeatureSet.eGeMAPSv01b,
-                opensmile.FeatureLevel.LowLevelDescriptors,
-            ):
-                feature_size = 23
-            case (
-                opensmile.FeatureSet.eGeMAPS
-                | opensmile.FeatureSet.eGeMAPSv01b
-                | opensmile.FeatureSet.eGeMAPSv02,
-                opensmile.FeatureLevel.Functionals,
-            ):
-                feature_size = 88
-            case opensmile.FeatureSet.eGeMAPSv02, opensmile.FeatureLevel.LowLevelDescriptors:
-                feature_size = 25
-            case opensmile.FeatureSet.emobase, opensmile.FeatureLevel.Functionals:
-                feature_size = 988
-            case opensmile.FeatureSet.emobase, opensmile.FeatureLevel.LowLevelDescriptors:
-                feature_size = 26
-            case _:
-                raise ValueError(f"unknown feature_set: {feature_set}/{feature_level}")
+        if feature_set == opensmile.FeatureSet.ComParE_2016 and feature_level == opensmile.FeatureLevel.Functionals:
+            feature_size = 6373
+        elif feature_set == opensmile.FeatureSet.ComParE_2016:
+            feature_size = 65
+        elif (
+            feature_set in [opensmile.FeatureSet.GeMAPS, opensmile.FeatureSet.GeMAPSv01b]
+            and feature_level == opensmile.FeatureLevel.Functionals
+        ):
+            feature_size = 62
+        elif (
+            feature_set in [opensmile.FeatureSet.GeMAPS, opensmile.FeatureSet.GeMAPSv01b]
+            and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+        ):
+            feature_size = 18
+        elif (
+            feature_set in [opensmile.FeatureSet.eGeMAPS, opensmile.FeatureSet.eGeMAPSv01b]
+            and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+        ):
+            feature_size = 23
+        elif (
+            feature_set
+            in [opensmile.FeatureSet.eGeMAPS, opensmile.FeatureSet.eGeMAPSv01b, opensmile.FeatureSet.eGeMAPSv02]
+            and feature_level == opensmile.FeatureLevel.Functionals
+        ):
+            feature_size = 88
+        elif (
+            feature_set == opensmile.FeatureSet.eGeMAPSv02
+            and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+        ):
+            feature_size = 25
+        elif feature_set == opensmile.FeatureSet.emobase and feature_level == opensmile.FeatureLevel.Functionals:
+            feature_size = 988
+        elif (
+            feature_set == opensmile.FeatureSet.emobase and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+        ):
+            feature_size = 26
+        else:
+            raise ValueError(f"unknown feature_set: {feature_set}/{feature_level}")
 
         yield opensmile.Smile(feature_set=feature_set, feature_level=feature_level), feature_size
+
+
+def get_batch_size(feature_size, batch_size):
+    if feature_size < 30:
+        batch_size = batch_size
+    if feature_size > 30 and feature_size < 90:
+        batch_size = batch_size // 2
+    if feature_size == 988:
+        batch_size = batch_size // 4
+    if feature_size == 6373:
+        batch_size = batch_size // 8
+    return batch_size
+
+
+# def get_all_audio_methods_if(ignore_methods: Sequence[str] = ()):
+#     for feature_set, feature_level in product(opensmile.FeatureSet, opensmile.FeatureLevel):
+#         if feature_set.name in ignore_methods or feature_level.name in ignore_methods:
+#             continue
+#         if (
+#             feature_level == opensmile.FeatureLevel.LowLevelDescriptors_Deltas
+#             and feature_set != opensmile.FeatureSet.ComParE_2016
+#         ):
+#             continue
+
+#         if (
+#             feature_set == opensmile.FeatureSet.ComParE_2016
+#             and feature_level == opensmile.FeatureLevel.Functionals
+#         ):
+#             feature_size = 6373
+#         elif feature_set == opensmile.FeatureSet.ComParE_2016:
+#             feature_size = 65
+#         elif (
+#             feature_set in [opensmile.FeatureSet.GeMAPS, opensmile.FeatureSet.GeMAPSv01b]
+#             and feature_level == opensmile.FeatureLevel.Functionals
+#         ):
+#             feature_size = 62
+#         elif (
+#             feature_set in [opensmile.FeatureSet.GeMAPS, opensmile.FeatureSet.GeMAPSv01b]
+#             and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+#         ):
+#             feature_size = 18
+#         elif (
+#             feature_set in [opensmile.FeatureSet.eGeMAPS, opensmile.FeatureSet.eGeMAPSv01b]
+#             and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+#         ):
+#             feature_size = 23
+#         elif (
+#             feature_set
+#             in [opensmile.FeatureSet.eGeMAPS, opensmile.FeatureSet.eGeMAPSv01b, opensmile.FeatureSet.eGeMAPSv02]
+#             and feature_level == opensmile.FeatureLevel.Functionals
+#         ):
+#             feature_size = 88
+#         elif (
+#             feature_set == opensmile.FeatureSet.eGeMAPSv02
+#             and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+#         ):
+#             feature_size = 25
+#         elif (
+#             feature_set == opensmile.FeatureSet.emobase
+#             and feature_level == opensmile.FeatureLevel.Functionals
+#         ):
+#             feature_size = 988
+#         elif (
+#             feature_set == opensmile.FeatureSet.emobase
+#             and feature_level == opensmile.FeatureLevel.LowLevelDescriptors
+#         ):
+#             feature_size = 26
+#         else:
+#             raise ValueError(f"unknown feature_set: {feature_set}/{feature_level}")
+
+#         yield opensmile.Smile(feature_set=feature_set, feature_level=feature_level), feature_size
--- src/extractor/dataset/audio.py
+++ src/extractor/dataset/audio.py
@@ -21,3 +21,30 @@ class AudioFeaturesDataset(Dataset):
 
     def __len__(self):
         return len(self.items)
+
+
+class AudioFeaturesDataset_tensor(Dataset):  # 将特征字典转为tensor形式
+    def __init__(self, features_dict, *, use_cuda=False):
+        self.features_dict = features_dict
+        self.use_cuda = use_cuda
+        self.items = list(features_dict.items())
+
+    def __getitem__(self, index):
+        (label, _), features = self.items[index]
+        features = torch.tensor(features, dtype=torch.float32)
+        length = len(features)
+        if self.use_cuda:
+            return (
+                features.cuda(),
+                torch.tensor(label, dtype=torch.int64).cuda(),
+                torch.tensor(length, dtype=torch.int64).cuda(),
+            )
+        else:
+            return (
+                features,
+                torch.tensor(label, dtype=torch.int64),
+                torch.tensor(length, dtype=torch.int64),
+            )  # 返回特征和标签和长度
+
+    def __len__(self):
+        return len(self.items)
